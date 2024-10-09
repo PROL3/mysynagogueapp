429776e05160d8a80edf626b2e5436d3f39921a4
liav <161258307+PROL3@users.noreply.github.com>
2024-10-09 11:04:49 +0300

    Update app.js
    res.setHeader('Access-Control-Allow-Origin', '*'); // מאפשר לכל מקור

--- app.js
+++ app.js
@@ -22,7 +22,14 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // מאפשר שליחת עוגיות
   }));
-  
+  app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // מאפשר לכל מקור
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 
 
 app.use(express.json()); // מנתח בקשות JSON
