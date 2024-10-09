dfe7ff4a4aac9906109c6b416c40be8790240d84
liav <liavramadi17@gmail.com>
2024-10-10 01:45:19 +0300

    change db

--- schemas/user.js
+++ schemas/user.js
@@ -17,18 +17,18 @@ const UserSchema = new mongoose.Schema({
   totalDonated: { type: mongoose.Schema.Types.Decimal128, default: 0.0 }, // Change to Decimal128
   totalOwed: { type: mongoose.Schema.Types.Decimal128, default: 0.0 }, // Change to Decimal128
   monthlyDonations: { // שדה חדש עבור תרומות לפי חודשים
-    ינואר: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    פבואר: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    מרץ: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    אפריל: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    מאי: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    יוני: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    יולי: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    אוגוסט: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    ספטמבר: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    אוקטובר: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    נובמבר: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
-    דצמבר: {  type: mongoose.Schema.Types.Decimal128, default: 0.0 },
+    ינואר: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    פבואר: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    מרץ: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    אפריל: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    מאי: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    יוני: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    יולי: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    אוגוסט: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    ספטמבר: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    אוקטובר: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    נובמבר: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
+    דצמבר: { type: mongoose.Schema.Types.Decimal128, default: () => mongoose.Types.Decimal128.fromString('0.0') },
   }
 }, { timestamps: true });
 
